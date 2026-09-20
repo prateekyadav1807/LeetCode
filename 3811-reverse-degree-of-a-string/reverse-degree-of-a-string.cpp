df0931fdc6006880d405b4d05c6791df5b8cd012
Prateek Yadav <153089848+prateekyadav1807@users.noreply.github.com>
2026-09-20 11:39:52 +0530

    Time: 4 ms (9.52%) | Memory: 9.7 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+
+        for (int i = 0; i < (int)s.length(); i++) {
+
+            char ch = s[i];
+
+            int alphabet = ch - 'a' + 1;
+
+            int reverse = 27 - alphabet;
+
+            int position = i + 1;
+
+            int product = reverse * position;
+
+            sum = sum + product;
+        }
+
+        return sum;
+    }
+};
